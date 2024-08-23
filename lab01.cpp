9a291f7305c3ebc41bf4490a8a4a1d73e3f32683
Xtal85-92 <mercadojahir16@gmail.com>
2024-08-23 16:36:09 +0000

    Commit

---
+++ lab01.cpp
@@ -0,0 +1,13 @@
+
+// Jahir Mercado (Xtal85-92, Jahir_Mercado)
+// A program to practice modifications and compilation
+
+#include <iostream>
+
+int main() {
+  std::cout << "Hello World!" << std::endl;
+  std::cout << "This is a 1!" << std::endl;
+  
+  return 0;
+}
+
